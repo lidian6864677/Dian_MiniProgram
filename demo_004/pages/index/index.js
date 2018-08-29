f0d0e944e8e42d54120e3aa6b1f3483015815f11
Dian <lidian@tuhu.cn>
2018-08-29 11:51:14 +0800

    Demo_001  ---  Demo004

---
+++ demo_004/pages/index/index.js
@@ -0,0 +1,74 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    markers: [{
+      iconPath: "http://webapi.amap.com/theme/v1.3/markers/b/mark_bs.png",
+      id: 0,
+      latitude: 31.131877,
+      longitude: 121.398325,
+      width: 10,
+      height: 20
+    }],
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+  
+})
